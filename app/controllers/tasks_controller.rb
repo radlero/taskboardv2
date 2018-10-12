d43941394857c9e3a4b52f82dc23122959485317
radlero <radoslaw.lewandowski@arnoldclark.com>
2018-10-12 11:51:33 +0100

    added new tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,45 @@
+class TasksController < ApplicationController
+http_basic_authenticate_with name: "rad", password: "123", except: [:index, :show]
+
+  def index
+      @tasks = Task.all
+    end
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+def create
+      @task = Task.new(task_params)
+
+      @task.save
+      redirect_to @task
+end
+
+def destroy
+  @task = Task.find(params[:id])
+  @task.destroy
+
+  redirect_to tasks_path
+end
+
+def update
+  @task= Task.find(params[:id])
+
+  if @task.update(task_params)
+    redirect_to @task
+  else
+    render 'edit'
+  end
+end
+      private
+  def task_params
+    params.require(:task).permit(:task, :time, :station, :importance)
+
+  end
+  end
