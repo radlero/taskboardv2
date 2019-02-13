500e4c22d10e37e45797873578362ec48b97d269
radlero <radoslaw.lewandowski@arnoldclark.com>
2019-02-13 11:51:49 +0000

    routes problem resolved

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
-  resources :tasks, :station1 ,:station2
+  get '/station1', to: 'station1#index'
+  get '/station2', to: 'station2#index'
+  resources :tasks, :station1, :station2
   root 'welcome#index'
 
 
