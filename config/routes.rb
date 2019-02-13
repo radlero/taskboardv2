e441e2d5ac5fd65246ed8f4d24848dfa8e66bc80
radlero <radoslaw.lewandowski@arnoldclark.com>
2019-02-13 11:47:19 +0000

    routes repairedv3

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :tasks
-
+  resources :tasks, :station1 ,:station2
   root 'welcome#index'
-  get  '/station1', to: 'station1#index'
-  get '/station2', to: 'station2#index'
+
 
 end
