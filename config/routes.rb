bafafb96065b090b16c33fa4812c70bafb49d060
radlero <radoslaw.lewandowski@arnoldclark.com>
2019-02-13 12:00:05 +0000

    asf

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
+  
   get 'welcome/index'
   get '/station1', to: 'station1#index'
   get '/station2', to: 'station2#index'
-  resources :tasks, :station1, :station2
+  resources :tasks
   root 'welcome#index'
 
 
