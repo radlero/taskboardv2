8e283724502757528651ce1a6862bb161631ec6a
radlero <radoslaw.lewandowski@arnoldclark.com>
2019-02-14 11:40:48 +0000

    added stylesheets and chasis

--- app/controllers/station1_controller.rb
+++ app/controllers/station1_controller.rb
@@ -1,4 +1,5 @@
 class Station1Controller < ApplicationController
+  http_basic_authenticate_with name: "rad", password: "uws", except: [:index, :show]
     def index
         @tasks = Task.stations_order.priority_order.where(station: 'station1').limit(8)
         
--- app/controllers/station2_controller.rb
+++ app/controllers/station2_controller.rb
@@ -1,4 +1,5 @@
 class Station2Controller < ApplicationController
+  http_basic_authenticate_with name: "rad", password: "uws", except: [:index, :show]
     def index
         @tasks = Task.stations_order.priority_order.where(station: 'station2').limit(8)
         
